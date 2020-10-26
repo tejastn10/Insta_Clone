a6b9007e6feece39091de9ba9f2f1c94e75c3c88
Tejas Nikhar <tejastn10@gmail.com>
2020-10-26 22:02:50 +0530

    📦 UPDATE: Login and SignUp Screens

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,3 +1,4 @@
+import 'package:Insta_Clone/screens/auth/signup.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -21,97 +22,101 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Instagram",
-              style: TextStyle(
-                fontSize: 50.0,
-                fontFamily: "Billabong",
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Instagram",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: "Billabong",
+                ),
               ),
-            ),
-            Form(
-              key: _formkey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: "Name"),
-                      validator: (input) => input.trim().isEmpty
-                          ? "Please enter a valid Name!"
-                          : null,
-                      onSaved: (input) => _name = input,
+              Form(
+                key: _formkey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Name"),
+                        validator: (input) => input.trim().isEmpty
+                            ? "Please enter a valid Name!"
+                            : null,
+                        onSaved: (input) => _name = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: "Email"),
-                      validator: (input) => !input.contains('@')
-                          ? "Please enter a valid email!"
-                          : null,
-                      onSaved: (input) => _email = input,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Email"),
+                        validator: (input) => !input.contains('@')
+                            ? "Please enter a valid email!"
+                            : null,
+                        onSaved: (input) => _email = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: "Password"),
-                      validator: (input) => input.length < 6
-                          ? "Must be atleast 6 characters long!"
-                          : null,
-                      onSaved: (input) => _password = input,
-                      obscureText: true,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Password"),
+                        validator: (input) => input.length < 6
+                            ? "Must be atleast 6 characters long!"
+                            : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.white,
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: () => Navigator.pop(context),
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        "Go to Sign Up",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.white,
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: () =>
+                            Navigator.pushNamed(context, SignUp.id),
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "Go to Sign Up",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -1,4 +1,3 @@
-import 'package:Insta_Clone/screens/auth/login.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -22,86 +21,89 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Instagram",
-              style: TextStyle(
-                fontSize: 50.0,
-                fontFamily: "Billabong",
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Instagram",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: "Billabong",
+                ),
               ),
-            ),
-            Form(
-              key: _formkey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: "Email"),
-                      validator: (input) => !input.contains('@')
-                          ? "Please enter a valid email!"
-                          : null,
-                      onSaved: (input) => _email = input,
+              Form(
+                key: _formkey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Email"),
+                        validator: (input) => !input.contains('@')
+                            ? "Please enter a valid email!"
+                            : null,
+                        onSaved: (input) => _email = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: "Password"),
-                      validator: (input) => input.length < 6
-                          ? "Must be atleast 6 characters long!"
-                          : null,
-                      onSaved: (input) => _password = input,
-                      obscureText: true,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Password"),
+                        validator: (input) => input.length < 6
+                            ? "Must be atleast 6 characters long!"
+                            : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.white,
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: () => Navigator.pushNamed(context, Login.id),
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        "Go to Login",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.white,
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: () => Navigator.pop(context),
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "Go to Login",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
