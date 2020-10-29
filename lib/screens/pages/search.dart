f64a2b145389550752050ebf9f0c1745480a10e1
Tejas Nikhar <tejastn10@gmail.com>
2020-10-29 17:01:50 +0530

    📦 NEW: Search Page

---
+++ lib/screens/pages/search.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("search"),
+      ),
+    );
+  }
+}
