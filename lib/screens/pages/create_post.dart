180d5009edb22f923a31801bf74a45abac3fd562
Tejas Nikhar <tejastn10@gmail.com>
2020-10-29 17:01:25 +0530

    📦 NEW: Create Post Page

---
+++ lib/screens/pages/create_post.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreatePost extends StatefulWidget {
+  @override
+  _CreatePostState createState() => _CreatePostState();
+}
+
+class _CreatePostState extends State<CreatePost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("create post"),
+      ),
+    );
+  }
+}
