320294a5fe56b8040c0f3cdb36cc02addcfce2f4
Tejas Nikhar <tejastn10@gmail.com>
2020-10-30 13:01:19 +0530

    💀 SKELETON: Profile Page

--- lib/screens/pages/profile.dart
+++ lib/screens/pages/profile.dart
@@ -9,8 +9,119 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("profile"),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "12",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                "posts",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "120",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                "following",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "98",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text(
+                                "followers",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Container(
+                        width: 200.0,
+                        child: FlatButton(
+                          color: Colors.blue,
+                          child: Text(
+                            "Edit Profile",
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.white),
+                          ),
+                          onPressed: () => print("Edit Profile"),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30.0,
+              vertical: 10.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Username",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Container(
+                  height: 80.0,
+                  child: Text(
+                    "Bio",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                Divider()
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
