817191aa59c613160dfc13a06787d175a160a4a8
Tejas Nikhar <tejastn10@gmail.com>
2020-10-30 21:30:54 +0530

    📦 NEW: Database service File

---
+++ lib/services/database.dart
@@ -0,0 +1,12 @@
+import 'package:Insta_Clone/models/user.dart';
+import 'package:Insta_Clone/utilities/constants.dart';
+
+class Database {
+  static void updateUser(User user) {
+    usersRef.doc(user.id).update({
+      "name": user.name,
+      "bio": user.bio,
+      "photoImageURL": user.profileImageURL,
+    });
+  }
+}
