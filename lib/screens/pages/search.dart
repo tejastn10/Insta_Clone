ab748dc74b45bb241a8140da5de5bab7cefac0c1
Tejas Nikhar <tejastn10@gmail.com>
2020-11-04 05:05:58 +0530

    📦 UPDATE: Search Page with search funtionality

--- lib/screens/pages/search.dart
+++ lib/screens/pages/search.dart
@@ -1,3 +1,8 @@
+import 'package:Insta_Clone/models/user.dart';
+import 'package:Insta_Clone/screens/pages/profile.dart';
+import 'package:Insta_Clone/services/database.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Search extends StatefulWidget {
@@ -6,12 +11,93 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController _searchController = TextEditingController();
+  Future<QuerySnapshot> _users;
+
+  _buildUserTile(User user) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: user.profileImageURL.isEmpty
+            ? AssetImage("assets/images/default_user_image.jpg")
+            : CachedNetworkImageProvider(user.profileImageURL),
+        radius: 20.0,
+      ),
+      title: Text(user.name),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => Profile(
+            userId: user.id,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _clearSearch() {
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _searchController.clear());
+    setState(() {
+      _users = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("search"),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+            border: InputBorder.none,
+            hintText: "Search",
+            prefixIcon: Icon(
+              Icons.search,
+              size: 30.0,
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: _clearSearch,
+            ),
+            filled: true,
+          ),
+          onSubmitted: (input) {
+            if (input.isNotEmpty) {
+              setState(() {
+                _users = Database.searchUsers(input);
+              });
+            }
+          },
+        ),
       ),
+      body: _users == null
+          ? Center(
+              child: Text("Search..."),
+            )
+          : FutureBuilder(
+              future: _users,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.data.documents.length == 0) {
+                  return Center(
+                    child: Text("No Users found!, Please try again"),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    User user = User.fromDoc(snapshot.data.documents[index]);
+                    return _buildUserTile(user);
+                  },
+                );
+              },
+            ),
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:Insta_Clone/models/user.dart';
 import 'package:Insta_Clone/utilities/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database {
   static void updateUser(User user) {
@@ -8,5 +9,12 @@ class Database {
       "bio": user.bio,
       "profileImageURL": user.profileImageURL,
     });
+  }
+
+  static Future<QuerySnapshot> searchUsers(String name) {
+    Future<QuerySnapshot> users =
+        usersRef.where("name", isGreaterThanOrEqualTo: name).get();
+
+    return users;
   }
 }
