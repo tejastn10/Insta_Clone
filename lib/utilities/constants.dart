debd62a9952e6330e2669e32f0c3afa862ec5401
Tejas Nikhar <tejastn10@gmail.com>
2020-11-05 19:33:54 +0530

    📦 NEW: Post Collection Reference

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,5 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 final _firestore = FirebaseFirestore.instance;
 
 final usersRef = _firestore.collection("users");
+final postsRef = _firestore.collection("posts");
 
 final storageRef = FirebaseStorage.instance.ref();
