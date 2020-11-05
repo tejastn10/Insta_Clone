19ea1eeeeee75eb801db6b5df7eb4b06e1d05df3
Tejas Nikhar <tejastn10@gmail.com>
2020-11-05 21:05:29 +0530

    📦 NEW: Followers & Following Collection Reference

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -5,5 +5,7 @@ final _firestore = FirebaseFirestore.instance;
 
 final usersRef = _firestore.collection("users");
 final postsRef = _firestore.collection("posts");
+final followersRef = _firestore.collection("followers");
+final followingRef = _firestore.collection("following");
 
 final storageRef = FirebaseStorage.instance.ref();
