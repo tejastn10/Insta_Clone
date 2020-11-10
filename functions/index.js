c44bdbd5582ea447a971a0764b4f101c6de02baf
Tejas Nikhar <tejastn10@gmail.com>
2020-11-10 15:00:59 +0530

    📦 NEW: Cloud Fucntions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,69 @@
-const functions = require('firebase-functions');
+import { firestore as functions } from "firebase-functions";
+import { initializeApp, firestore as admin } from "firebase-admin";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+initializeApp();
+
+exports.onFollowUser = functions
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onCreate(async (_, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    const followedUserPostRef = admin()
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts");
+    const userFeedRef = admin()
+      .collection("feeds")
+      .doc(followerId)
+      .collection("userFeed");
+
+    const followedUserPostsSnapshot = await followedUserPostRef.get();
+    followedUserPostsSnapshot.forEach((doc) => {
+      if (doc.exists) {
+        userFeedRef.doc(doc.id).set(doc.data());
+      }
+    });
+  });
+
+exports.onUnfollowUser = functions
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onDelete(async (_, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    const userFeedRef = admin()
+      .collection("feeds")
+      .doc(followerId)
+      .collection("userFeed")
+      .where("authorId", "==", userId);
+
+    const userPostsSnapshot = await userFeedRef.get();
+    userPostsSnapshot.forEach((doc) => {
+      if (doc.exists) {
+        doc.ref.delete();
+      }
+    });
+  });
+
+exports.onUploadPost = functions
+  .document("/posts/{userId}/usersPosts/{postId}")
+  .onCreate(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    const userFollowersRef = admin()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+
+    const userFollowersSnapshot = await userFollowersRef.get();
+    userFollowersSnapshot.forEach((doc) => {
+      admin()
+        .collection("feeds")
+        .doc(doc.id)
+        .collection("userFeed")
+        .doc(postId)
+        .set(snapshot.data());
+    });
+  });
